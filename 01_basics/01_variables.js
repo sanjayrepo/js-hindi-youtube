a6216c49147d0e57b340d49a7ec63624eb829d8c
sanjayrepo <59650254+sanjayrepo@users.noreply.github.com>
2023-08-21 15:13:00 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="sanjay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId =2
+accountEmail = "abc@gmail.com"
+accountPassword = "212121"
+accountcity ="Bangaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"
+
+console.log(3+3); 
+console.log("Sanjay")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof undefined);
+console.log(typeof null);
