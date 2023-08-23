716bee167185402145c07ac17bd41ec685e6245b
sanjayrepo <59650254+sanjayrepo@users.noreply.github.com>
2023-08-23 08:31:04 +0000

    datatype summery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+// Primitive
+/*
+ 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+*/
+
+// Reference(Non Premitive)
+
+/*
+    array, Objects, Function
+*/
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const bigNumber = 13241235245234535634563456666n
+
+
+const heros =[ "Shaktiman", "naagaraj"]
