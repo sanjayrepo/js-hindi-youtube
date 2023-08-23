d2341326d1608644d28c7adce3d4d550b7940b43
sanjayrepo <59650254+sanjayrepo@users.noreply.github.com>
2023-08-23 06:41:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = true
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" =>33
+//"33abc" => Nan
+// true =>1; false =>0
+
+
+let isLoggedIn = "Sanjay"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
