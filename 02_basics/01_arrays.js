821750b0a24612993b4f86bdfda8988fee5f7686
sanjayrepo <59650254+sanjayrepo@users.noreply.github.com>
2023-08-25 05:06:04 +0000

    array practice

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+//array
+
+const myArray = [0,1,2,3,4,5,6]
+
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArray2 = new Array(1,2,3,4)
+console.log(myArray[1]);
+
+// array methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+// const newArray = myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+
+
+//slice ,splice
+
+console.log("A ",myArray)
+const myn1 = myArray.splice(1,3)
---
+++ 02_basics/02_array.js
@@ -0,0 +1,15 @@
+const marvel_heros = ["thor", "Ironman", "Spiderman"]
+const dc_heros =["superman","flash","spiderman", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros =[...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+
+const another_array= [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+let real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
